8c4cea9af9088f74bba36dedef3e11a2eab33b0d
Aman Arora <amanarora6767@gmail.com>
2019-06-07 17:02:58 +0530

    edit and delete post completed

--- src/main/java/technicalblog/model/Post.java
+++ src/main/java/technicalblog/model/Post.java
@@ -1,33 +1,56 @@
 package technicalblog.model;
-
+import javax.persistence.*;
 import java.util.Date;
 
-public class Post {
-    private String title;
-    private String body;
-    private Date date;
 
-    public String getTitle() {
-        return title;
+    @Entity
+    @Table(name = "posts")
+    public class Post {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        @Column(name = "id")
+        private Integer id;
+
+        @Column(name = "tittle")
+        private String title;
+
+        @Column(name = "body")
+        private String body;
+        @Column (name = "date")
+        private Date date;
+
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getBody() {
+            return body;
+        }
+
+        public void setBody(String body) {
+            this.body = body;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-}
---
+++ src/main/java/technicalblog/service/PostService.java
@@ -0,0 +1,53 @@
+package technicalblog.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import technicalblog.model.Post;
+import technicalblog.repository.PostRepository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import javax.persistence.TypedQuery;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class PostService {
+    @PersistenceUnit(unitName = "techblog")
+    private EntityManagerFactory emf;
+    @Autowired
+    private PostRepository postRepository;
+    public List<Post> getAllPosts() {
+
+
+        return postRepository.getAllPosts();
+
+
+    }
+
+    public Post getOnePost() {
+
+          return postRepository.getLatestPost();
+    }
+    public void createPost(Post newPost) {
+        postRepository.createPost(newPost);
+        System.out.println("New Post: "+newPost);
+    }
+    public Post getPost(Integer postId) {
+        return postRepository.getPost(postId);
+    }
+
+    public void updatePost(Post updatedPost) {
+        updatedPost.setDate(new Date());
+        postRepository.updatePost(updatedPost);
+    }
+
+    public void deletePost(Integer postId) {
+        postRepository.deletePost(postId);
+    }
+
+}
